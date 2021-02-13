40901638cab3de120f5c003d194525b5dff2d7fe
Otavio Landim <francisco.conrado@zup.com.br>
2021-02-13 17:05:06 -0300

    moving url

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,10 +3,12 @@ module.exports = (app) => {
     .get(app.controllers.userController.findAll)
     .post(app.controllers.userController.create);
 
-  // app.route('/accounts')
-  //   .get(app.controllers.accountController.getAll)
-  //   .post(app.controllers.accountController.create);
-//
-  // app.route('/accounts/:id')
-  //   .get(app.controllers.accountController.get);
+  app.route('/accounts')
+    .get(app.controllers.accountController.getAll)
+    .post(app.controllers.accountController.create);
+
+  app.route('/accounts/:id')
+    .get(app.controllers.accountController.get)
+    .put(app.controllers.accountController.update)
+    .delete(app.controllers.accountController.remove);
 };
